99158bc998ba360462ae613acf89c8b86c8e702f
TensionX <44634819+TensionX@users.noreply.github.com>
2023-09-28 09:13:47 -0500

    improvements

---
+++ cocktails.js
@@ -0,0 +1,283 @@
+function cocktails(){
+    return [
+        {
+            name: "Sunset Sangria",
+            recipe: {
+                ingredients: [
+                    "3 oz red wine",
+                    "1 oz orange juice",
+                    "1/2 oz brandy",
+                    "1/4 oz simple syrup",
+                    "Various fruits (oranges, lemons, apples) for garnish"
+                ]
+            },
+            preparation: [
+                "In a pitcher, combine wine, orange juice, brandy, and syrup.",
+                "Stir gently to mix the ingredients.",
+                "Add chopped fruits to the pitcher and let it sit for about 30 minutes to infuse.",
+                "Serve chilled in glasses, ensuring each has a mix of the fruit."
+            ]
+        },
+        {
+            name: "Coconut Bliss",
+            recipe: {
+                ingredients: [
+                    "2 oz coconut rum",
+                    "1 oz pineapple juice",
+                    "1/2 oz lime juice",
+                    "Pineapple slice for garnish"
+                ]
+            },
+            preparation: [
+                "Fill a shaker with ice.",
+                "Add coconut rum, pineapple juice, and lime juice to the shaker.",
+                "Shake vigorously for about 15 seconds.",
+                "Strain the mixture into a glass.",
+                "Garnish with a slice of pineapple."
+            ]
+        },
+        {
+            name: "Spiced Orchid",
+            recipe: {
+                ingredients: [
+                    "2 oz gin",
+                    "1 oz orchid extract",
+                    "1/2 oz spiced syrup",
+                    "1/4 oz lemon juice",
+                    "Orchid petal for garnish"
+                ]
+            },
+            preparation: [
+                "Fill a cocktail shaker with ice.",
+                "Pour in the gin, orchid extract, spiced syrup, and lemon juice.",
+                "Shake well until the shaker feels cold.",
+                "Strain the cocktail into a glass.",
+                "Garnish with an orchid petal."
+            ]
+        },
+        {
+            name: "Tropical Tonic",
+            recipe: {
+                ingredients: [
+                    "2 oz vodka",
+                    "1 oz passion fruit puree",
+                    "1/2 oz lime juice",
+                    "Tonic water",
+                    "Lime wheel for garnish"
+                ]
+            },
+            preparation: [
+                "In a glass filled with ice, pour the vodka, passion fruit puree, and lime juice.",
+                "Stir gently to mix.",
+                "Top up with tonic water.",
+                "Garnish with a lime wheel."
+            ]
+        },
+        {
+            name: "Berry Bourbon Burst",
+            recipe: {
+                ingredients: [
+                    "2 oz bourbon",
+                    "1 oz mixed berry compote",
+                    "1/2 oz lemon juice",
+                    "1/4 oz simple syrup",
+                    "Mixed berries for garnish"
+                ]
+            },
+            preparation: [
+                "Place the mixed berry compote at the bottom of a glass.",
+                "Muddle the berries until they release their juices.",
+                "Add ice to the glass.",
+                "In a separate shaker, combine bourbon, lemon juice, and simple syrup. Shake well.",
+                "Pour the shaken mixture over the muddled berries.",
+                "Garnish with additional mixed berries."
+            ]
+        },
+        {
+            name: "Minty Mocha Martini",
+            recipe: {
+                ingredients: [
+                    "2 oz coffee liqueur",
+                    "1 oz vodka",
+                    "1/2 oz creme de menthe",
+                    "1/4 oz chocolate syrup",
+                    "Mint leaf and coffee bean for garnish"
+                ]
+            },
+            preparation: [
+                "In a shaker filled with ice, combine the coffee liqueur, vodka, creme de menthe, and chocolate syrup.",
+                "Shake vigorously until well chilled.",
+                "Strain into a martini glass.",
+                "Garnish with a mint leaf and a coffee bean."
+            ]
+        },
+        {
+            name: "Peachy Keen Punch",
+            recipe: {
+                ingredients: [
+                    "2 oz peach schnapps",
+                    "1 oz rum",
+                    "1 oz orange juice",
+                    "1/2 oz cranberry juice",
+                    "Orange wheel and cherry for garnish"
+                ]
+            },
+            preparation: [
+                "Fill a glass with ice cubes.",
+                "Pour peach schnapps, rum, orange juice, and cranberry juice over the ice.",
+                "Stir gently to combine the ingredients.",
+                "Garnish with an orange wheel and a cherry."
+            ]
+        },
+        {
+            name: "Cucumber Collins",
+            recipe: {
+                ingredients: [
+                    "2 oz gin",
+                    "1 oz fresh cucumber juice",
+                    "1/2 oz lemon juice",
+                    "1/4 oz simple syrup",
+                    "Soda water",
+                    "Cucumber slice for garnish"
+                ]
+            },
+            preparation: [
+                "Fill a shaker with ice cubes.",
+                "Add gin, cucumber juice, lemon juice, and simple syrup.",
+                "Shake well until the mixture is cold.",
+                "Strain into a tall glass filled with ice.",
+                "Top up with soda water.",
+                "Garnish with a slice of cucumber."
+            ]
+        },
+        {
+            name: "Lavender Lemon Fizz",
+            recipe: {
+                ingredients: [
+                    "2 oz vodka",
+                    "1 oz lavender syrup",
+                    "1/2 oz lemon juice",
+                    "Soda water",
+                    "Lavender sprig for garnish"
+                ]
+            },
+            preparation: [
+                "In a glass filled with ice, combine vodka, lavender syrup, and lemon juice.",
+                "Stir to mix.",
+                "Fill the rest of the glass with soda water.",
+                "Gently stir again.",
+                "Garnish with a sprig of lavender."
+            ]
+        },
+        {
+            name: "Raspberry Rumble",
+            recipe: {
+                ingredients: [
+                    "2 oz rum",
+                    "1 oz raspberry puree",
+                    "1/2 oz lime juice",
+                    "1/4 oz simple syrup",
+                    "Fresh raspberries for garnish"
+                ]
+            },
+            preparation: [
+                "Place the raspberry puree at the bottom of a shaker.",
+                "Add rum, lime juice, and simple syrup.",
+                "Fill the shaker with ice and shake until well chilled.",
+                "Strain into a glass over fresh ice.",
+                "Garnish with fresh raspberries."
+            ]
+        },
+        {
+            name: "Kiwi Kiss",
+            recipe: {
+                ingredients: [
+                    "2 oz white rum",
+                    "1 oz kiwi puree",
+                    "1/2 oz lime juice",
+                    "1/4 oz simple syrup",
+                    "Kiwi slice for garnish"
+                ]
+            },
+            preparation: [
+                "In a shaker, combine the rum, kiwi puree, lime juice, and simple syrup.",
+                "Add ice and shake well until chilled.",
+                "Strain into a glass filled with ice.",
+                "Garnish with a kiwi slice."
+            ]
+        },
+        {
+            name: "Fig & Fennel Fusion",
+            recipe: {
+                ingredients: [
+                    "2 oz vodka",
+                    "1 oz fig jam",
+                    "1/4 oz fennel syrup",
+                    "1/2 oz lemon juice",
+                    "Sprig of fennel for garnish"
+                ]
+            },
+            preparation: [
+                "In a shaker, combine the vodka, fig jam, fennel syrup, and lemon juice.",
+                "Shake vigorously until well combined.",
+                "Strain into a chilled cocktail glass.",
+                "Garnish with a sprig of fennel."
+            ]
+        },
+        {
+            name: "Maple Mule",
+            recipe: {
+                ingredients: [
+                    "2 oz bourbon",
+                    "1 oz maple syrup",
+                    "1/2 oz lime juice",
+                    "Ginger beer to top",
+                    "Lime wheel and mint sprig for garnish"
+                ]
+            },
+            preparation: [
+                "In a mule mug filled with ice, pour in the bourbon, maple syrup, and lime juice.",
+                "Stir gently to mix.",
+                "Top with ginger beer.",
+                "Garnish with a lime wheel and a sprig of mint."
+            ]
+        },
+        {
+            name: "Pear & Pepper Pleasure",
+            recipe: {
+                ingredients: [
+                    "2 oz cognac",
+                    "1 oz pear nectar",
+                    "1/4 oz black pepper syrup",
+                    "1/2 oz lemon juice",
+                    "Pear slice and cracked black pepper for garnish"
+                ]
+            },
+            preparation: [
+                "In a shaker, combine the cognac, pear nectar, black pepper syrup, and lemon juice.",
+                "Shake well until chilled.",
+                "Strain into a glass filled with ice.",
+                "Garnish with a pear slice and sprinkle with cracked black pepper."
+            ]
+        },
+        {
+            name: "Tarragon Tease",
+            recipe: {
+                ingredients: [
+                    "2 oz gin",
+                    "1 oz tarragon-infused simple syrup",
+                    "1/2 oz lemon juice",
+                    "Soda water to top",
+                    "Tarragon sprig for garnish"
+                ]
+            },
+            preparation: [
+                "In a glass filled with ice, pour the gin, tarragon syrup, and lemon juice.",
+                "Stir to mix.",
+                "Top with soda water.",
+                "Garnish with a sprig of fresh tarragon."
+            ]
+        },
+        
+    ]
+}
